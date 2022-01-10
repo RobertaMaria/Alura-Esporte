00aaf681bf7f65fc0a323b91f75f9b9c74bc9869
RobertaMaria <robertaji26@gmail.com>
2022-01-10 15:21:18 -0300

    configura funçao cadastra para receber usuario

--- app/src/main/java/br/com/alura/aluraesporte/ui/viewmodel/CadastroUsuarioViewModel.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/viewmodel/CadastroUsuarioViewModel.kt
@@ -1,11 +1,14 @@
 package br.com.alura.aluraesporte.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import br.com.alura.aluraesporte.model.Usuario
 import br.com.alura.aluraesporte.repository.FirebaseAuthRepository
+import br.com.alura.aluraesporte.repository.Resource
 
 class CadastroUsuarioViewModel(private val repository: FirebaseAuthRepository) : ViewModel() {
 
-    fun cadastra(email: String, senha: String) {
-        repository.cadastraUsuario(email, senha)
+    fun cadastra(usuario: Usuario): LiveData<Resource<Boolean>> {
+        return repository.cadastraUsuario(usuario)
     }
 }
